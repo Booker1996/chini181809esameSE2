ad250ea338462d5fa0f9de61ad54e30ceed7e806
Ubuntu 17.10 <lorenzo.chini96@gmail.com>
2018-01-26 11:35:31 +0100

    Modify checker

--- checker.js
+++ checker.js
@@ -2,6 +2,16 @@
 const fetch = require('node-fetch')
 
 function check(url, invocationParameters,  expectedResultData, expectedResultStatus) {
+    var urlGET = url + '?' + 'lato1=' + invocationParameters.lato1 + '&' + 'lato2=' + invocationParameters.lato2;
+    console.log(urlGET);
+    return fetch(url).then(response => {
+            console.log("Okay!");
+
+            if(response.status===expectedResultStatus){
+                console.log('Lo status è giusto!');
+
+            }
+    }).catch(error => console.log("Si è verificato un errore!"));
 
     const checkResult = { // this is the object you need to set and return
         urlChecked: url,
@@ -28,3 +38,5 @@ function compareResults(expected, actual) {
 }
 
 module.exports = check
+
+check('https://example.com',{lato1:3,lato2:5},{area:15},200);
